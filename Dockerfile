c22785af4653b43095e524e6493185bb999681b5
Reese Wilson <reese@shinymayhem.com>
2015-03-27 15:32:19 -0700

    simplifying

--- Dockerfile
+++ Dockerfile
@@ -2,21 +2,16 @@ FROM node:0.12-slim
 
 RUN \
   npm install -g bower && \
+  npm install -g forever && \
   npm install -g nodemon && \
-  mkdir -p /opt/node && \
-  mkdir -p /var/www
+  mkdir -p /var/www && \
+  useradd -ms /sbin/nologin node && \
+  chown -R node:node /var/www
 
-COPY deploy/run.sh /opt/deploy/run.sh
-RUN \
-	chmod +x /opt/deploy/*.sh
+WORKDIR /var/www/
 
-VOLUME /var/www/
+EXPOSE 3000
 
-WORKDIR /opt/node
+USER node
 
-ENV COPY_TO_VOLUME true
-ENV INDEX_DIR /var/www
-
-EXPOSE 80
-
-CMD /bin/bash /opt/deploy/run.sh
+CMD npm install && npm start
--- deploy/run.sh
+++ deploy/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+whoami
+
 #copy node modules, etc to volume the server runs from
 if [ "$COPY_TO_VOLUME" = "true" ]; then
 	echo "Copying from /opt/node to /var/www \n"
@@ -7,9 +9,12 @@ else
 	echo "Leaving contents of /var/www alone \n"
 fi
 
-if [ -f ${INDEX_DIR}/index.js ];
-then
-    cd ${INDEX_DIR}; nodemon --watch ${INDEX_DIR} ${INDEX_DIR}/index.js
-else
-    echo "No index.js file found in ${INDEX_DIR}"
-fi
+#add 'start' script to package.json to specify how server should be run
+npm start
+
+#if [ -f ${INDEX_DIR}/index.js ];
+#then
+#    cd ${INDEX_DIR}; nodemon --watch ${INDEX_DIR} ${INDEX_DIR}/index.js
+#else
+#    echo "No index.js file found in ${INDEX_DIR}"
+#fi
